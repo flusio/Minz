flusio/Minz

commit 4fd60b3e6a6812c0f2bc7cf10fab9abd0d8e1218

Set default url options port wisely

src/Configuration.php
@@ -21,7 +21,8 @@ namespace Minz;
  * - app_name: it must be the same as the base namespace of the application
  * - url_options, with:
  *   - host: the domain name pointing to your server
- *   - port: the listening port of your server (default is 80)
+ *   - port: the listening port of your server (default is set to 443 if
+ *           protocol is https, else 80)
  *   - path: URI path to your application (default is /)
  *   - protocol: the protocol used by your server (default is http)
  *
@@ -139,11 +140,13 @@ class Configuration
         }
 
         $default_url_options = [
-            'port' => 80,
             'path' => '/',
             'protocol' => 'http',
         ];
         self::$url_options = array_merge($default_url_options, $url_options);
+        if (!isset(self::$url_options['port'])) {
+            self::$url_options['port'] = $url_options['protocol'] === 'https' ? 443 : 80;
+        }
 
         $default_data_path = $app_path . '/data';
         self::$data_path = self::getDefault(

commit 3c4751a5646b92e1fac75519797676e9ef47d5a7

fix: Fix call to url protocol in configuration
I called the protocol on the wrong variable. Everything was working fine
(a bit per chance) except an annoying warning.

Bug introduced in commit 4fd60b3e6a6812c0f2bc7cf10fab9abd0d8e1218

src/Configuration.php
@@ -145,7 +145,7 @@ class Configuration
         ];
         self::$url_options = array_merge($default_url_options, $url_options);
         if (!isset(self::$url_options['port'])) {
-            self::$url_options['port'] = $url_options['protocol'] === 'https' ? 443 : 80;
+            self::$url_options['port'] = self::$url_options['protocol'] === 'https' ? 443 : 80;
         }
 
         $default_data_path = $app_path . '/data';