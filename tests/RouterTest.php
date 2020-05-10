flusio/Minz

commit b69a786131abc63296e7b6588de37dcc20c06482

Allow wildcard parameter in router patterns

src/Router.php
@@ -226,13 +226,15 @@ class Router
         $pattern_exploded = explode('/', $pattern);
         $path_exploded = explode('/', $path);
 
-        if (count($pattern_exploded) !== count($path_exploded)) {
-            return false;
-        }
-
         for ($i = 0; $i < count($pattern_exploded); $i++) {
             $pattern_element = $pattern_exploded[$i];
             $path_element = $path_exploded[$i];
+
+            // If the pattern is *, the rest of the path is considered as
+            // matching
+            if ($pattern_element && $pattern_element === '*') {
+                return true;
+            }
 
             // In the pattern /rabbits/:id, :id is a variable name, which is
             // replaced by a real value in the URI (e.g. /rabbits/42).
@@ -244,7 +246,8 @@ class Router
             }
         }
 
-        return true;
+        // we still have to check that the path has no more elements than the pattern
+        return count($pattern_exploded) === count($path_exploded);
     }
 
     /**
@@ -266,6 +269,14 @@ class Router
         for ($i = 0; $i < count($pattern_exploded); $i++) {
             $pattern_element = $pattern_exploded[$i];
             $path_element = $path_exploded[$i];
+
+            if ($pattern_element && $pattern_element === '*') {
+                // the rest of the path matches with the pattern wildcard, so
+                // we rebuild this part of the path
+                $rest_of_path = array_slice($path_exploded, $i);
+                $parameters['*'] = implode('/', $rest_of_path);
+                break;
+            }
 
             $pattern_is_variable = $pattern_element && $pattern_element[0] === ':';
             if (!$pattern_is_variable) {

tests/RouterTest.php
@@ -158,6 +158,20 @@ class RouterTest extends TestCase
         $this->assertSame(['id' => '42'], $parameters);
     }
 
+    public function testMatchWithWildcard()
+    {
+        $router = new Router();
+        $router->addRoute('get', '/assets/*', 'assets#serve');
+
+        list(
+            $action_pointer,
+            $parameters
+        ) = $router->match('get', '/assets/path/to/an/asset.css');
+
+        $this->assertSame('assets#serve', $action_pointer);
+        $this->assertSame(['*' => 'path/to/an/asset.css'], $parameters);
+    }
+
     public function testMatchFailsIfNotMatchingVia()
     {
         $this->expectException(Errors\RouteNotFoundError::class);

commit 5327db3d287a92f98b6bd0d9b3d0d77c39e923c0

fix: Handle router patterns longer than paths
This bug was introduced in b69a786. Before, we were checking both
strings were the same length before the for-loop, so they had the same
number of elements. With the introduction of the wildcard parameter, we
must iterate immediately since the wildcard can match several
elements of the path. It means we also need to check the path element
exists before using it!

src/Router.php
@@ -227,6 +227,10 @@ class Router
         $path_exploded = explode('/', $path);
 
         for ($i = 0; $i < count($pattern_exploded); $i++) {
+            if (!isset($path_exploded[$i])) {
+                return false;
+            }
+
             $pattern_element = $pattern_exploded[$i];
             $path_element = $path_exploded[$i];
 

tests/RouterTest.php
@@ -172,6 +172,17 @@ class RouterTest extends TestCase
         $this->assertSame(['*' => 'path/to/an/asset.css'], $parameters);
     }
 
+    public function testMatchFailsIfPatternIsLongerThanPath()
+    {
+        $this->expectException(Errors\RouteNotFoundError::class);
+        $this->expectExceptionMessage('Path "get /rabbits" doesn’t match any route.');
+
+        $router = new Router();
+        $router->addRoute('get', '/rabbits/:id', 'rabbits#show');
+
+        $router->match('get', '/rabbits');
+    }
+
     public function testMatchFailsIfNotMatchingVia()
     {
         $this->expectException(Errors\RouteNotFoundError::class);