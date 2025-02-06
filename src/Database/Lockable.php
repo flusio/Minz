5d365190b95fe26fd3b03048d7d86dbfdd0e5477
Marien Fressinaud <dev@marienfressinaud.fr>
2025-02-06 11:25:30 +0100

    dev: Update the license copyright year

--- src/Database/Lockable.php
+++ src/Database/Lockable.php
@@ -1,7 +1,7 @@
 <?php
 
 // This file is part of Minz.
-// Copyright 2020-2024 Marien Fressinaud
+// Copyright 2020-2025 Marien Fressinaud
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 namespace Minz\Database;
