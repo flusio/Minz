fe0f648831d0b576650748d47a9bdd45c059c50b
Marien Fressinaud <marien+dev@fressinaud.fr>
2025-12-26 22:16:07 +0100

    fix: Reset Twig instance during tests

--- src/Tests/ApplicationHelper.php
+++ src/Tests/ApplicationHelper.php
@@ -51,6 +51,10 @@ trait ApplicationHelper
     #[\PHPUnit\Framework\Attributes\BeforeClass]
     public static function loadApplication(): void
     {
+        // This avoids to register extensions multiple times (normally done in
+        // the Application class constructor) as it would fail.
+        \Minz\Template\Twig::reset();
+
         $app_name = \Minz\Configuration::$app_name;
         $application_class_name = "\\{$app_name}\\Application";
         try {
