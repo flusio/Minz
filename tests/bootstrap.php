2930e92ac7f5798725ea98b81463828cb7d3a796
Marien Fressinaud <marien+dev@fressinaud.fr>
2026-01-21 17:31:19 +0100

    chore: Update the copyright year in file headers

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 // This file is part of Minz.
-// Copyright 2020-2025 Marien Fressinaud
+// Copyright 2020-2026 Marien Fressinaud
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 require dirname(__DIR__) . '/vendor/autoload.php';
